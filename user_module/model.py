3710664caa7dadd991d927ba7535962d962a7df9
yuhao.he <hawl.yuhao.he@gmail.com>
2023-06-10 12:35:24 +0800

    fix

--- user_module/model.py
+++ user_module/model.py
@@ -6,5 +6,5 @@ from sqlmodel import Field, SQLModel
 class User(SQLModel):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(..., max_length=32)
-    email: str = Field(..., max_length=64)
+    email: str = Field(..., max_lengtlsh=64)
     hashed_password: str = Field(..., max_length=64)
